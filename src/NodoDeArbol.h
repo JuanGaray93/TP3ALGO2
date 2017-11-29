71935a3819a54d041b56e8e28dbcdad0f9408a62
Juan <juangaray93@gmail.com>
2017-11-29 15:46:29 -0300

    Separadas las declaraciones de la implementacion en nododearbol

--- src/NodoDeArbol.h
+++ src/NodoDeArbol.h
@@ -20,77 +20,105 @@ public:
 	/*
 	 * Post: Crea nuevo nodo sin hijos y sin contenido.
 	 */
-	NodoDeArbol(){
-		hijos = new Lista<NodoDeArbol>;
-		contenido = 0;
-	}
+	NodoDeArbol();
 
 	/*
 	 * Post: Crea un nuevo nodo sin hijos con el contenido pasado
 	 */
-	NodoDeArbol(T nuevoContenido){
-		hijos = new Lista<NodoDeArbol>;
-		contenido = new T;
-		*contenido = nuevoContenido;
-	}
+	NodoDeArbol(T nuevoContenido);
 
 	/*
 	 * Post: Crea un nuevo nodo con contenido e hijos dados.
 	 */
-	NodoDeArbol(T nuevoContenido, Lista<NodoDeArbol> nuevosHijos){
-		hijos = new Lista<NodoDeArbol>(nuevosHijos);
-		contenido = new T;
-		*contenido = nuevoContenido;
-	}
-
+	NodoDeArbol(T nuevoContenido, Lista<NodoDeArbol> nuevosHijos);
 
 	/*
 	 * Pre: El nodo existe
 	 * Post: el nodo pasa a tener el contenido dado.
 	 * 		 Si ya tenia contenido, se sobreescribe y se pierden los datos anteriores
 	 */
-	void asignarContenido(T nuevoContenido){
-		if (contenido==0){
-			contenido = new T;
-		}
-		*contenido = nuevoContenido;
-	}
+	void asignarContenido(T nuevoContenido);
 
 	/*
 	 * Pre: El nodo existe
 	 * Post: El nodo tiene de hijo el nodo pasado
 	 */
-	void asignarNuevoHijo(NodoDeArbol nuevoHijo){
-		hijos->agregar(nuevoHijo);
-	}
+	void asignarNuevoHijo(NodoDeArbol nuevoHijo);
 
 	/*
 	 * Pre: El nodo existe
 	 * Post: devuelve la cantidad de hijos que tiene el nodo.
 	 */
-	int devolverCantidadDeHijos(){
-		return ( hijos->contarElementos() );
-	}
+	int devolverCantidadDeHijos();
 
 	/*
 	 * Pre: Se le asigno contenido a traves del constructor o de asignarContenido.
 	 * Post: Devuelve el contenido de este nodo.
 	 */
-	T devolerContenido(){
-		return *contenido;
-	}
+	T devolverContenido();
 
 	/*
 	 * Pre: Existe el nodo.
 	 * 		NumeroDeHijo esta entre 1 y devolverCantidadDeHijos()
 	 * Post: Devuelve direccion del nodo del hijo pedido.
 	 */
-	NodoDeArbol* devolverHijo(int numeroDeHijo){
-		return &(hijos->obtener(numeroDeHijo));
-	}
+	NodoDeArbol* devolverHijo(int numeroDeHijo);
 
 
 };
+
+template <class T>
+NodoDeArbol<T>::NodoDeArbol(){
+	hijos = new Lista<NodoDeArbol>;
+	contenido = 0;
+}
+
+template <class T>
+NodoDeArbol<T>::NodoDeArbol(T nuevoContenido){
+	hijos = new Lista<NodoDeArbol>;
+	contenido = new T;
+	*contenido = nuevoContenido;
+}
+
+template <class T>
+NodoDeArbol<T>::NodoDeArbol(T nuevoContenido, Lista<NodoDeArbol> nuevosHijos){
+	hijos = new Lista<NodoDeArbol>(nuevosHijos);
+	contenido = new T;
+	*contenido = nuevoContenido;
+}
+
+template <class T>
+void NodoDeArbol<T>::asignarContenido(T nuevoContenido){
+	if (contenido==0){
+		contenido = new T;
+	}
+	*contenido = nuevoContenido;
+}
+
+template <class T>
+void NodoDeArbol<T>::asignarNuevoHijo(NodoDeArbol nuevoHijo){
+	hijos->agregar(nuevoHijo);
+}
+
+template <class T>
+int NodoDeArbol<T>::devolverCantidadDeHijos(){
+	return ( hijos->contarElementos() );
+}
+
+template <class T>
+T NodoDeArbol<T>::devolverContenido(){
+	return *contenido;
+}
+
+template <class T>
+NodoDeArbol* NodoDeArbol<T>::devolverHijo(int numeroDeHijo){
+	return &(hijos->obtener(numeroDeHijo));
+}
+
+
+
+
+
 
 
 #endif /* NODODEARBOL_H_ */
