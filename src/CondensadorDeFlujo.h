f640a5f6c8caa245dfc591ed0ea2f0a3e46399a7
Juan <juangaray93@gmail.com>
2017-12-01 03:08:48 -0300

    Doquiumenteiyon

--- src/CondensadorDeFlujo.h
+++ src/CondensadorDeFlujo.h
@@ -31,64 +31,60 @@ private:
 public:
 
 	/*
-	 * Post:
+	 * Post: Se crea un condensador nuevo con un arbol de jugadas de solo un elemento vacio
 	 */
 	CondensadorDeFlujo();
 
 	/*
 	 * Pre: El condensador existe
-	 * Post: El nodo
+	 * Post: Se suma un nuevo nodo al arbol, partiendo del nodo actual.
 	 */
 	void agregarNodo(NodoDeArbol<Jugada>* nodoNuevo);
 
-	/* Pre: La cantidad de nodos a retroceder son menos que devolverProfundidadActual()
+	/* Pre: La cantidad de nodos a retroceder es menos que devolverProfundidadActual()
 	 *
 	 */
 	void retroceder(int cantidadDeNodos);
-	/*
-	 *
+
+	/* Pre: El condensador existe.
+	 * Post: Se rehacen las jugadas. Si la cantidad de jugadas pedida excede la cantidad de jugadas futuras hechas,
+	 * 		 Se detiene el avance en la ultima jugada hecha. Se le pregunta al jugador cuando hay una bifurcacion
+	 * 		 de posibles jugadas a repetir.
 	 */
 	void avanzar(int cantidadDeNodos);
 
-	/*
-	 *
+	/* Pre: El condensador existe
+	 * Post: Devuelve la cantidad de jjugadas que se hicieron desde la primera hasta la actual
 	 */
 	int devolverProfundidadActual();
 
-	/*
-	 *
+	/* Pre: El condensador existe y se corrio inicializarJuego()
+	 * Post: Inicia el ciclo de juego y sigue hasta que algun jugador gane
 	 */
-	void ejecutarJuego(){
+	void ejecutarJuego();
 
-		juego->avanzarTurno();
-		while(sigueElJuego){
-
-			juego->declararTurno();
-			juego->tomarJugada();
-			juego->avanzarTurno();
-			if(huboCambios){
-				//Guardo en un nodo nuevo el estado actual de la partida
-				NodoDeArbol<Jugada>* aGuardar = new NodoDeArbol<Jugada>(nodoActual, jugadaActual);
-				agregarNodo(aGuardar);
-			}
-
-			avanzar(1);
-			sigueElJuego = !(juego->terminoLaPartida());
-		};
-
-	}
-
-	/*
-	 *
+	/* Pre: el condensador existe
+	 * Post: Se crea la instancia de juego. Se puede ejecutarJuego();
 	 */
-	void inicializarJuego(int dificultad, int numeroDeJugadores, int filas, int columnas, std::string* nombresDeJugadores){
-		juego = new Juego(dificultad, numeroDeJugadores, filas, columnas, nombresDeJugadores);
-	}
+	void inicializarJuego(int dificultad, int numeroDeJugadores, int filas, int columnas, std::string* nombresDeJugadores);
 
-	/*
-	 *
+	/* Pre: El condensador existe. Se inicio el juego. nodoActual->devolverCantidadDeHijos() > 1
+	 * Post: Presenta un resumen de cada jugada futura elegible y pregunta que jugada repetir.
+	 * 		 Devuelve el numero de la jugada elegida.
 	 */
 	int preguntarQueHijoSeguir();
+
+	/*
+	 * Post: Pide que el usuario ingrese un numero hasta recibir uno mayor a cero.
+	 * 		 Devuelve el numero ingresado.
+	 */
+	int pedirNumero(std::string mensaje);
+
+	/*
+	 * Post: Pide que el usuario ingrese un numero hasta recibir uno mayor a cero y menor o igual al numero maximo
+	 * 		 Devuelve el numero ingresado.
+	 */
+	int pedirNumero(std::string mensaje, int numeroMaximo);
 
 
 };
@@ -123,6 +119,7 @@ void CondensadorDeFlujo::avanzar(int cantidadDeNodos){
 
 	/* Probablemente se pueda optimizar o emprolijar  */
 	if(nodosPosibles != 0 && cantidadDeNodos > 0){
+
 		/*SUMAR CAMBIOS SOBRE JUEGO*/
 		int hijoASeguir;
 
@@ -141,18 +138,79 @@ void CondensadorDeFlujo::avanzar(int cantidadDeNodos){
 }
 
 int CondensadorDeFlujo::preguntarQueHijoSeguir(){
-	//preguntar numero (copiar metodo)
-	return 1;
+	std::cout << "Hay varias opciones para avanzar!" << std::endl;
+
+	/*MOSTRAR DATOS DE CADA JUGADA*/
+
+	return pedirNumero("Que camino desea seguir?");
 }
 
 int CondensadorDeFlujo::devolverProfundidadActual(){
 	return nodoActual->devolverProfundidad();
 }
 
+void CondensadorDeFlujo::ejecutarJuego(){
+
+	juego->avanzarTurno();
+	while(sigueElJuego){
+
+		juego->declararTurno();
+		juego->tomarJugada();
+		juego->avanzarTurno();
+		if(huboCambios){
+			//Guardo en un nodo nuevo el estado actual de la partida
+			NodoDeArbol<Jugada>* aGuardar = new NodoDeArbol<Jugada>(nodoActual, jugadaActual);
+			agregarNodo(aGuardar);
+		}
+
+		avanzar(1);
+		sigueElJuego = !(juego->terminoLaPartida());
+	};
+
+}
+
+void CondensadorDeFlujo::inicializarJuego(int dificultad, int numeroDeJugadores, int filas, int columnas, std::string* nombresDeJugadores){
+		juego = new Juego(dificultad, numeroDeJugadores, filas, columnas, nombresDeJugadores);
+	}
 
 
+int CondensadorDeFlujo::pedirNumero(std::string mensaje){
+	int numeroIngresado;
+	std::cout << mensaje << std::endl;
+	std::cin >> numeroIngresado;
 
+	while (std::cin.fail()){
+		std::cin.clear();
+		std::cin.ignore();
+		std::cin >> numeroIngresado;
+	}
 
+	if(numeroIngresado <= 0){
+
+		std::cout << "El numero debe ser mayor a cero!" << std::endl;
+		numeroIngresado =  pedirNumero(mensaje);
+	}
+	return numeroIngresado;
+}
+
+int CondensadorDeFlujo::pedirNumero(std::string mensaje, int numeroMaximo){
+	int numeroIngresado = 0;
+	std::cout << mensaje << std::endl;
+	std::cin >> numeroIngresado;
+
+	while (std::cin.fail()){
+		std::cin.clear();
+		std::cin.ignore();
+		std::cin >> numeroIngresado;
+	}
+
+	if(numeroIngresado <= 0 || numeroIngresado > numeroMaximo){
+
+		std::cout << "Numero no valido! " << std::endl;
+		numeroIngresado = pedirNumero(mensaje, numeroMaximo);
+	}
+	return numeroIngresado;
+}
 
 
 
